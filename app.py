3a20a44d59612db62944d3099726e2da9b23ce7d
Shifullah <ahmed.shifullah@gmail.com>
2019-03-12 14:52:53 +0600

    2nd Ip fetching added ..

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ app = Flask(__name__)
 def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
 
+@app.route('/user_ip', methods=['GET'])
+def get_tasks():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+    else:
+        return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
+
 @app.route("/") # only "/"= Home page
 def index():
 
